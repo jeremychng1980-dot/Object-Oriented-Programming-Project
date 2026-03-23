04428488e978489041b4d091ebd08af0cec71f87
PpBeh <behjiazhi0000@gmail.com>
2026-03-23 14:03:49 +0000

    Test2

---
+++ src/Car.java
@@ -0,0 +1,7 @@
+public class Car{
+    private String carID = "C0001";
+
+    public Car(){
+        carID = "";
+    };
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import Util.p;
+
+
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Welcome to AMCS2204!");
+
+        p.println("Hello World");
+        
+        p.println("");
+
     }
 } 
