607e81f518e6344bcfd7971a5207a89d7cd8930f
PpBeh <behjiazhi0000@gmail.com>
2026-03-24 07:59:40 +0000

    Car Class version 1.0

--- src/Car.java
+++ src/Car.java
@@ -1,7 +1,85 @@
 public class Car{
-    private String carID = "C0001";
+    private String carID ;          // auto generate no need use input
+    private static int carCount = 1;    // use to generate the carID
+    private String plateNumber;
+    private String model;
+    private String brand;
+    private double dailyRate;
+    private int seatingCapacity;
+    private int mileage;
+    private String status;
+    private double fuelLevel;
+
+    // constructor
 
     public Car(){
-        carID = "";
+        this("", "","",0.0, 0, 0, "", 0.0);
     };
+
+    public Car(String plateNumber, String model, String brand, double dailyRate, 
+               int seatingCapacity, int mileage, String status, double fuelLevel){
+        carID = String.format("CA%04d", carCount);
+        this.plateNumber = plateNumber;
+        this.model = model;
+        this.brand = brand;
+        this.dailyRate = dailyRate;
+        this.seatingCapacity = seatingCapacity;
+        this.mileage = mileage;
+        this.status = status;
+        this.fuelLevel = fuelLevel;
+        carCount++;
+    }
+
+    // getter
+    public String getCarID(){
+        return carID;
+    }
+    
+    public String getPlateNumber(){
+        return plateNumber;
+    }
+    
+    public String getModel(){
+        return model;
+    }
+
+    public String getBrand(){
+        return brand;
+    }
+
+    public double getDailyRate(){
+        return dailyRate;
+    }
+
+    public int getSeatingCapacity(){
+        return seatingCapacity;
+    }
+
+    public int getMileage(){
+        return mileage;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public double getFuelLevel(){
+        return fuelLevel;
+    }
+
+    // setter
+    public void setDailyRate(double dailyRate){
+        this.dailyRate = dailyRate;
+    }
+
+    public void setMileage(int mileage){
+        this.mileage = mileage;
+    }
+
+    public void setStatus(String status){
+        this.status = status;
+    }
+
+
+    
 }
--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome to AMCS2204!");
-
         p.println("Hello World");
         
-        p.println("");
+        Car c1 = new Car("PQR2123", "Vios", "Toyota", 40.0, 4, 25009, "Available", 280);
+
+        p.println("Test print car id: " + c1.getCarID());
+        
 
     }
 } 
