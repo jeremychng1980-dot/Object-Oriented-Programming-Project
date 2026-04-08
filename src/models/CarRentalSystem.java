3eee9dd048d6dcd293491da3e1622d2ecca6586e
Jeremy <jeremychng1980@gmail.com>
2026-04-08 05:20:35 +0000

    Added CarRentalSystem class to manage car rentals and reservations. This class includes methods for adding cars, making reservations, and checking availability. The implementation is basic and can be expanded with additional features such as user management and payment processing in the future.

---
+++ src/models/CarRentalSystem.java
@@ -0,0 +1,31 @@
+package models;
+
+public class CarRentalSystem {
+    private Car[] cars;
+    private Customer[] cust;
+    private Payment[] payment;
+
+    private int carCount;
+    private int custCount;
+    private int paymentCount;
+
+    public CarRentalSystem() {
+        cars = new Car[100];
+        cust = new Customer[100];
+        payment = new Payment[100];
+
+        carCount = 0;
+        custCount = 0;
+        paymentCount = 0;
+    }
+
+    public CarRentalSystem(int maxCars, int maxCustomers, int maxPayments) {
+        cars = new Car[maxCars];
+        cust = new Customer[maxCustomers];
+        payment = new Payment[maxPayments];
+
+        carCount = 0;
+        custCount = 0;
+        paymentCount = 0;
+    }
+}
